311a3537b55e654f740b04ccb5a6e9509dc52574
macarojasb <mrojasbustamante@smith.edu>
2016-02-28 21:26:52 -0500

    2.29.16

---
+++ Documents/xCode/Twitter/Twitter/LoginfViewController.swift
@@ -0,0 +1,53 @@
+//
+//  LoginfViewController.swift
+//  Twitter
+//
+//  Created by Macarena Rojas on 2/22/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+import BDBOAuth1Manager
+
+class LoginfViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    
+        
+    }
+    
+    @IBAction func onLoginButtom(sender: AnyObject) {
+        let client = TwitterClient.sharedInstance
+        
+        client.login({ () -> () in
+            print("I've logged in!")
+            
+            self.performSegueWithIdentifier("loginSegue", sender: nil)
+            }) { (error: NSError) -> () in
+                print("Error: \(error.localizedDescription)")
+        }
+        
+        
+    }
+  
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
